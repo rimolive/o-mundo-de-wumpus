a0cd16a15014b931e837f4a2f92edd46fca7aafb
rimolive <ricardo.martinelli.oliveira@gmail.com>
2018-05-05 23:34:04 -0300

    Quase finalizado heuristica 2

--- src/heuristica-2.pl
+++ src/heuristica-2.pl
@@ -66,9 +66,15 @@ tem_wumpus(_, _, nao).
 agrega_conhecimento(brisa) :-
     cacador(X, Y, _),
     assertz(brisa_em(X, Y)).
+agrega_conhecimento(sem_brisa) :-
+        cacador(X, Y, _),
+        assertz(sem_brisa_em(X, Y)).
 agrega_conhecimento(fedor) :-
     cacador(X, Y, _),
     assertz(fedor_em(X, Y)).
+agrega_conhecimento(sem_fedor) :-
+    cacador(X, Y, _),
+    assertz(sem_fedor_em(X, Y)).
 
 % ---------------------------- %
 % Define heuristicas           %
@@ -83,34 +89,37 @@ heuristica([_, _, _, _, _], exit) :-
 
 heuristica([_, _, sim, _, _], pegar) :- !.
 
-heuristica([sim, _, _, _, _], [virar, E]) :-
+heuristica([sim, _, _, _, _], [virar, esquerda]) :-
     (
-        cacador(X, Y, D), D == leste, N is Y+1, nas_fronteiras(X, N), L is X+1, tem_fedor(X, N, nao), tem_fedor(L, Y, talvez);
-        cacador(X, Y, D), D == norte, O is X-1, nas_fronteiras(O, Y), N is Y+1, tem_fedor(O, Y, nao), tem_fedor(X, N, talvez);
-        cacador(X, Y, D), D == oeste, S is Y-1, nas_fronteiras(X, S), O is X-1, tem_fedor(X, S, nao), tem_fedor(O, Y, talvez);
-        cacador(X, Y, D), D == sul, L is X+1, nas_fronteiras(L, Y), S is Y-1, tem_fedor(L, Y, nao), tem_fedor(X, S, talvez)
+        cacador(X, Y, D), D == leste, N is Y+1, nas_fronteiras(X, N), L is X+1, tem_wumpus(X, N, nao), \+visitado(L, Y), tem_wumpus(L, Y, talvez);
+        cacador(X, Y, D), D == norte, O is X-1, nas_fronteiras(O, Y), N is Y+1, tem_wumpus(O, Y, nao), \+visitado(X, N), tem_wumpus(X, N, talvez);
+        cacador(X, Y, D), D == oeste, S is Y-1, nas_fronteiras(X, S), O is X-1, tem_wumpus(X, S, nao), \+visitado(O, Y), tem_wumpus(O, Y, talvez);
+        cacador(X, Y, D), D == sul, L is X+1, nas_fronteiras(L, Y), S is Y-1, tem_wumpus(L, Y, nao), \+visitado(X, S), tem_wumpus(X, S, talvez)
     ),
     write('O Wumpus está por aqui. '),
+    agrega_conhecimento(sem_brisa),
     agrega_conhecimento(fedor), !.
 
-heuristica([sim, _, _, _, _], [virar, D]) :-
+heuristica([sim, _, _, _, _], [virar, direita]) :-
     (
-        cacador(X, Y, D), D == leste, S is Y-1, nas_fronteiras(X, S), L is X+1, tem_fedor(X, S, nao), tem_fedor(L, Y, talvez);
-        cacador(X, Y, D), D == norte, L is X+1, nas_fronteiras(L, Y), N is Y+1, tem_fedor(L, Y, nao), tem_fedor(X, N, talvez);
-        cacador(X, Y, D), D == oeste, N is Y+1, nas_fronteiras(X, N), O is X-1, tem_fedor(X, N, nao), tem_fedor(O, Y, talvez);
-        cacador(X, Y, D), D == sul, O is X-1, nas_fronteiras(O, Y), S is Y-1, tem_fedor(O, Y, nao), tem_fedor(X, O, talvez)
+        cacador(X, Y, D), D == leste, S is Y-1, nas_fronteiras(X, S), L is X+1, tem_wumpus(X, S, nao), \+visitado(L, Y), tem_wumpus(L, Y, talvez);
+        cacador(X, Y, D), D == norte, L is X+1, nas_fronteiras(L, Y), N is Y+1, tem_wumpus(L, Y, nao), \+visitado(X, N), tem_wumpus(X, N, talvez);
+        cacador(X, Y, D), D == oeste, N is Y+1, nas_fronteiras(X, N), O is X-1, tem_wumpus(X, N, nao), \+visitado(O, Y), tem_wumpus(O, Y, talvez);
+        cacador(X, Y, D), D == sul, O is X-1, nas_fronteiras(O, Y), S is Y-1, tem_wumpus(O, Y, nao), \+visitado(X, S), tem_wumpus(X, S, talvez)
     ),
     write('O Wumpus está por aqui. '),
+    agrega_conhecimento(sem_brisa),
     agrega_conhecimento(fedor), !.
 
 heuristica([sim, _, _, _, _], adiante) :-
     (
-        cacador(X, Y, D), D == leste, L is X+1, nas_fronteiras(L, Y), tem_fedor(L, Y, nao);
-        cacador(X, Y, D), D == norte, N is Y+1, nas_fronteiras(X, N), tem_fedor(X, N, nao);
-        cacador(X, Y, D), D == oeste, O is X-1, nas_fronteiras(O, Y), tem_fedor(O, Y, nao);
-        cacador(X, Y, D), D == sul, S is Y-1, nas_fronteiras(X, S), tem_fedor(X, S, nao)
+        cacador(X, Y, D), D == leste, L is X+1, nas_fronteiras(L, Y), tem_wumpus(L, Y, nao);
+        cacador(X, Y, D), D == norte, N is Y+1, nas_fronteiras(X, N), tem_wumpus(X, N, nao);
+        cacador(X, Y, D), D == oeste, O is X-1, nas_fronteiras(O, Y), tem_wumpus(O, Y, nao);
+        cacador(X, Y, D), D == sul, S is Y-1, nas_fronteiras(X, S), tem_wumpus(X, S, nao)
     ),
     write('O Wumpus está por aqui. '),
+    agrega_conhecimento(sem_brisa),
     agrega_conhecimento(fedor), !.
 
 heuristica([_, sim, _, _, _], [virar, esquerda]) :-
@@ -121,6 +130,7 @@ heuristica([_, sim, _, _, _], [virar, esquerda]) :-
         cacador(X, Y, D), D == sul, L is X+1, nas_fronteiras(L, Y), S is Y-1, tem_buraco(L, Y, nao), \+visitado(X, S), tem_buraco(X, S, talvez)
     ),
     write('Tem um buraco por aqui. '),
+    agrega_conhecimento(sem_fedor),
     agrega_conhecimento(brisa), !.
 
 heuristica([_, sim, _, _, _], [virar, direita]) :-
@@ -131,6 +141,7 @@ heuristica([_, sim, _, _, _], [virar, direita]) :-
         cacador(X, Y, D), D == sul, O is X-1, nas_fronteiras(O, Y), S is Y-1, tem_buraco(O, Y, nao), \+visitado(X, S), tem_buraco(X, S, talvez)
     ),
     write('Tem um buraco por aqui. '),
+    agrega_conhecimento(sem_fedor),
     agrega_conhecimento(brisa), !.
 
 heuristica([_, sim, _, _, _], adiante) :-
@@ -141,19 +152,22 @@ heuristica([_, sim, _, _, _], adiante) :-
         cacador(X, Y, D), D == sul, S is Y-1, nas_fronteiras(X, S), tem_buraco(X, S, nao)
     ),
     write('Tem um buraco por aqui. '),
+    agrega_conhecimento(sem_fedor),
     agrega_conhecimento(brisa), !.
 
 heuristica([_, _, _, sim, _], [virar, direita]) :- !.
 
 heuristica([_, _, _, sim, _], [virar, esquerda]) :- !.
 
-
 % Volta pro comeco
-heuristica(_, [mover, X, Y]) :-
+heuristica(_, adiante) :-
     tem_ouro(sim),
-    write('Voltando... '),
-    % melhor_caminho(X, Y),
-    !.
+    write('Voltando... '), !.
+
+heuristica(_, [virar, direita]) :-
+        tem_ouro(sim),
+        cacador(X, Y, _), 
+        write('Voltando... '), !.
 
 heuristica(_, exit) :-
     cacador(X, Y, _),
